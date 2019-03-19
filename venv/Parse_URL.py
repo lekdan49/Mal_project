aaa402c1d2baac76e31be9466b388ab4dbebc918
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2019-03-19 14:55:53 +1100

    Done

--- venv/Parse_URL.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-r = requests.get('https://myanimelist.net/anime/6547')
-soup = BeautifulSoup(r.text, 'html.parser')
-score = soup.find_all('div', attrs={'class':'fl-l score'})
-new_data = score[0]
-print(new_data)
