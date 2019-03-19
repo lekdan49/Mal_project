c954b40442b0cdf84fc9dc36c9c4ff8d7f1c398d
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2019-03-19 12:51:11 +1100

    Initial commit

---
+++ Mak_url.py
@@ -0,0 +1,13 @@
+from requests.compat import urljoin
+
+for child in f:
+    url_joining = 'https://myanimelist.net/anime/'
+    new_var = str(child)
+    url_new = urljoin
+    #print(url_new(url_joining, new_var))
+# https://myanimelist.net/anime/37033
+# //*[@id="content"]/table/tbody/tr/td[2]/div[1]/table/tbody/tr[1]/td/div[1]/div[1]/div[1]/div[1]/div[1]/text()
+
+
+
+
---
+++ ReadXML.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+import shutil
+import os
+
+tree = ET.parse('animelist_1552948095_-_5559128.xml')
+root = tree.getroot()
+
+for child in root.findall('anime'):
+    anime_number = child.find('series_animedb_id').text
+    f = open("anidbid.txt", "a")
+    f.write("%r\n" %anime_number)
+    f.close()
+with open('anidbid.txt', 'r') as f, open('output.txt', 'w') as fo:
+    for line in f:
+        fo.write(line.replace('"', '').replace("'", ""))
+
+shutil.move("output.txt", "Done_ani/output.txt")
+os.remove("anidbid.txt")
+
