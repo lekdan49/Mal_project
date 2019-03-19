f0f99b263a4e1950c2c7113fd321e50d207042ce
lekdan49 <danindu19@gmail.com>
2019-03-19 15:03:12 +1100

    Update parse

--- Parse_URL.py
+++ Parse_URL.py
@@ -3,6 +3,17 @@ from bs4 import BeautifulSoup
 import re
 import urllib
 from urllib.request import urlopen
+f = open('NEW_URLS.txt', 'r')
+
+for child in f:
+    str(child)
+    r = requests.get(child)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    score = soup.find_all('div', attrs={'class': 'fl-l score'})
+    str(score)
+    # print(new_data)
+    decimal_val = re.findall(r"[-+]?\d*\.\d+|\d+", str(score))
+    
 
 
 # r = requests.get('https://myanimelist.net/anime/6547')
